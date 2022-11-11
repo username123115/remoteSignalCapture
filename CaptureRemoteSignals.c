01f44445cc675095a23d37d592e89bc9ac842f51
Daniel <mrpickles123115@gmail.com>
2022-11-11 08:11:09 -0700

    new file:   .gitignore 	new file:   CMakeLists.txt 	new file:   Capture.pio 	new file:   CaptureRemoteSignals.c

---
+++ CaptureRemoteSignals.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/gpio.h"
+#include "hardware/divider.h"
+#include "hardware/dma.h"
+#include "hardware/pio.h"
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart1
+#define BAUD_RATE 9600
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+
+// GPIO defines
+// Example uses GPIO 2
+#define GPIO 2
+
+
+
+
+int main()
+{
+    stdio_init_all();
+
+
+
+    puts("Hello, world!");
+
+    return 0;
+}
